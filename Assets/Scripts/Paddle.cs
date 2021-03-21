cca9c5db1e0727d8bf7ffd65214c5bd076002964
YunjinJo <black44jo@naver.com>
2021-03-21 22:48:30 +0900

    Paddle will not go off the screen

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
-
-    
 {
     // Start is called before the first frame update
 
     [SerializeField] float screenWidthInUnits = 16f;
-    
+    [SerializeField] float MinX = 1f;
+    [SerializeField] float MaxX = 15f;
+
+
     void Start()
     {
         
@@ -20,7 +21,8 @@ public class Paddle : MonoBehaviour
     {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Debug.Log(mousePosInUnits);      
-        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, MinX, MaxX);
         transform.position = paddlePos;
 
     }
