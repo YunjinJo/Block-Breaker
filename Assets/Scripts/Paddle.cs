4a8a1a4b009fec3f8695451f232af662b173659a
YunjinJo <black44jo@naver.com>
2021-03-21 22:00:31 +0900

    Bounciness 1 -> 1.5 Paddle will move current mouse position

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+
+    
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Debug.Log(mousePosInUnits);      
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+
+    }
+}
